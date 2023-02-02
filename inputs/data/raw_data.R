106132e842c5b9496e98bdc359cc56a63d7ceeb6
Will Davidson <w.davidson@mail.utoronto.ca>
2023-02-02 14:50:22 -0500

    data to directory

--- inputs/data/raw_data.R
+++
@@ -1,7 +0,0 @@
-#### Acquire ####
-food_bank <-
-  # Each package is associated with a unique id  found in the "For 
-  # Developers" tab of the relevant page from Open Data Toronto
-  # https://open.toronto.ca/dataset/toronto-s-dashboard-key-indicators/
-  list_package_resources(c6d64e9b-f85a-4084-be14-60cf18509203"") |>
-  get_resource()
